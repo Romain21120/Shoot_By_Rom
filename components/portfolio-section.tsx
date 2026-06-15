c874a57e75c1113edd29b7026e82bcfda86f58e9
Romain21120 <190848707+Romain21120@users.noreply.github.com>
2026-06-15 21:17:38 +0200

    on fix les bug videos

--- components/portfolio-section.tsx
+++ components/portfolio-section.tsx
@@ -135,7 +135,7 @@ const portfolioItems: PortfolioItem[] = [
     title: "Explosive",
     location: "Guatemala",
     aspect: "landscape",
-    src: "7dydEKOU5aQ",
+    src: "0CZ7bMNRWc0",
   },
 ]
 
